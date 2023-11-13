fa00100de640e5e25901abf277e9bbc4e54126e2
wonjun <wonjon0930@daum.net>
2023-11-14 00:38:30 +0900

    feat: add feature to check user input about menu count length is not more than 2

--- src/main/java/christmas/view/input/validator/MenuCountInputValidator.java
+++ src/main/java/christmas/view/input/validator/MenuCountInputValidator.java
@@ -1,7 +1,10 @@
 package christmas.view.input.validator;
 
+import static christmas.view.input.constant.InputNumberConstant.MENU_COUNT_MAX_INPUT_LENGTH;
+import static christmas.view.input.constant.InputSymbolConstant.MENU_COUNT_SYMBOL;
+
 public class MenuCountInputValidator extends BasicValidator {
     public void validate(final String count) {
-
+        super.validate(count, MENU_COUNT_SYMBOL.getSymbol(), MENU_COUNT_MAX_INPUT_LENGTH.getValue());
     }
 }
