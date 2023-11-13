f941f33f7604b5ac2be2fc2242f69b9bd391baf3
wonjun <wonjon0930@daum.net>
2023-11-13 13:44:55 +0900

    feat: add feature to remove blank in user input about reservation day

---
+++ src/main/java/christmas/view/input/constant/InputConstant.java
@@ -0,0 +1,6 @@
+package christmas.view.input.constant;
+
+public class InputConstant {
+    public static String BLANK = " ";
+    public static String VOID = "";
+}
--- src/main/java/christmas/view/input/parser/InputParser.java
+++ src/main/java/christmas/view/input/parser/InputParser.java
@@ -1,5 +1,8 @@
 package christmas.view.input.parser;
 
+import static christmas.view.input.constant.InputConstant.BLANK;
+import static christmas.view.input.constant.InputConstant.VOID;
+
 import christmas.view.input.validator.DayInputValidator;
 
 public class InputParser {
@@ -10,11 +13,19 @@ public class InputParser {
     }
 
     public int parseDay(String userInput) {
+        userInput = removeBlank(userInput);
         dayInputValidator.validate(userInput);
         return parseToInt(userInput);
     }
 
     private int parseToInt(String userInput) {
         return Integer.parseInt(userInput);
+    }
+
+    private String removeBlank(String userInput) {
+        if (userInput.contains(BLANK)) {
+            userInput = userInput.replace(BLANK, VOID);
+        }
+        return userInput;
     }
 }
