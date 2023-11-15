ce6e107c5c943bcb1cf9179369bc9baae569828d
wonjun <wonjon0930@daum.net>
2023-11-15 18:00:22 +0900

    test: add test code for DayInputValidator

--- src/test/java/christmas/parser/InputParserTest.java
+++ src/test/java/christmas/parser/InputParserTest.java
@@ -49,16 +49,6 @@ public class InputParserTest {
                         String.format(BasicInputExceptionMessageFormat.TOO_LONG_WITH_BLANKS_FORMAT.getFormat(), "날짜"));
     }
 
-    @DisplayName("공백 제거 후 날짜 길이가 3글자 이상이면, 예외가 발생한다.")
-    @ParameterizedTest
-    @CsvSource(value = {" 0    3    0 ", "0 0   2", "0 0   0  0 4"})
-    void 공백_제거_후_입력값이_3글자_이상이면_예외_발생(String userInput) {
-        assertThatThrownBy(() -> inputParser.parseReservationDay(userInput))
-                .isInstanceOf(BasicInputException.class)
-                .hasMessageContaining(
-                        String.format(BasicInputExceptionMessageFormat.TOO_LONG_FORMAT.getFormat(), "날짜"));
-    }
-
     @DisplayName("공백 제거 후 주문내역 길이가 1000글자 이상이면, 예외가 발생한다.")
     @Test
     void 공백_포함_주문내역_입력값이_1000글자_이상이면_예외_발생() {
